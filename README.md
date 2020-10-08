390e81f888447fff4412c4d8add9e0a32caf546c
YEOLLL <28512134+YEOLLL@users.noreply.github.com>
2020-10-08 23:41:16 +0800

    2020.10.07爬取更新

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # BaiWenPai
-爬取百闻牌卡面图片(来自官方wiki)
+爬取百闻牌卡面图片，来自[BiliBiliWiki](https://wiki.biligame.com/yysbwp/%E5%88%86%E7%B1%BB:%E5%BC%8F%E7%A5%9E)
+
--- main.py
+++ main.py
@@ -8,11 +8,13 @@ from urllib.parse import unquote
 response = requests.get('https://wiki.biligame.com/yysbwp/%E5%88%86%E7%B1%BB:%E5%BC%8F%E7%A5%9E')
 html = etree.HTML(response.text)
 href_list = html.xpath('//div[@class="mw-category-group"]/ul/li/a/@href')
+s = requests.session()
+
 for href in href_list:
     name = unquote(href.split('/')[2])
     print(name)
     href = 'https://wiki.biligame.com/' + href
-    response = requests.get(href).text
+    response = s.get(href).text
     html = etree.HTML(response)
     pic0 = html.xpath('//center/img/@src')[0]
     if os.path.exists('./pic/' + name) == False:
